c334f64a1bc4a25f345724c943840f72eeb30f74
Stephanie Adams <adamsstephaniem@gmail.com>
2017-05-19 16:51:37 -0400

    test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import mockMedia from '../../mockLocalStorage';
+import mockLocalStorage from '../../mockLocalStorage';
+import ItemList from '../ItemList/ItemList'
 
-let media = mockMedia();
-
-beforeEach(()=>{
-  localStorage.setItem('mediaCollection', JSON.stringify(media));
-})
+let media = mockLocalStorage();
+localStorage.setItem('mediaCollection', JSON.stringify(media));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('ItemList renders', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ItemList allMedia={media} addMediaItem={()=>console.log('add')} removeMediaItem={()=>{console.log('remove')}} />, div)
 });
